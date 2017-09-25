d31daaf49aafbf50f2b1d524cde17c63a7f6dd8c
Jakob Engberg-Pedersen <jakob@local-m.local>
2017-09-25 14:22:30 +0200

    made node.js and golang comunicate throw rabbitmq

--- interface/api/server.js
+++ interface/api/server.js
@@ -2,5 +2,7 @@ const open = require('amqplib').connect('amqp://rabbit')
 
 const mqCall = require("./funcs/mqCallGen")(open)
 
-mqCall("test", "I'm killing it")
+mqCall("rpc_queue", "20").then(res => {
+  console.log(res)
+})
 console.log("hello")
--- server/cal/main.go
+++ server/cal/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/streadway/amqp"
 )
@@ -9,6 +10,16 @@ import (
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fib(n-1) + fib(n-2)
 	}
 }
 
@@ -22,19 +33,26 @@ func main() {
 	defer ch.Close()
 
 	q, err := ch.QueueDeclare(
-		"hello", // name
-		false,   // durable
-		false,   // delete when unused
-		false,   // exclusive
-		false,   // no-wait
-		nil,     // arguments
+		"rpc_queue", // name
+		false,       // durable
+		false,       // delete when usused
+		false,       // exclusive
+		false,       // no-wait
+		nil,         // arguments
 	)
 	failOnError(err, "Failed to declare a queue")
+
+	err = ch.Qos(
+		1,     // prefetch count
+		0,     // prefetch size
+		false, // global
+	)
+	failOnError(err, "Failed to set QoS")
 
 	msgs, err := ch.Consume(
 		q.Name, // queue
 		"",     // consumer
-		true,   // auto-ack
+		false,  // auto-ack
 		false,  // exclusive
 		false,  // no-local
 		false,  // no-wait
@@ -46,10 +64,28 @@ func main() {
 
 	go func() {
 		for d := range msgs {
-			log.Printf("Received a message: %s", d.Body)
+			n, err := strconv.Atoi(string(d.Body))
+			failOnError(err, "Failed to convert body to integer")
+
+			log.Printf(" [.] fib(%d)", n)
+			response := fib(n)
+
+			err = ch.Publish(
+				"",        // exchange
+				d.ReplyTo, // routing key
+				false,     // mandatory
+				false,     // immediate
+				amqp.Publishing{
+					ContentType:   "text/plain",
+					CorrelationId: d.CorrelationId,
+					Body:          []byte(strconv.Itoa(response)),
+				})
+			failOnError(err, "Failed to publish a message")
+
+			d.Ack(false)
 		}
 	}()
 
-	log.Printf(" [*] Waiting for messages. To exit press CTRL+C Hello")
+	log.Printf(" [*] Awaiting RPC requests")
 	<-forever
 }
