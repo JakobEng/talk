715e713f99c7d6f28b9facbea4032e82681b9df7
JakobEng <jakobeng@fastmail.com>
2017-09-28 00:35:48 +0200

    Added a proxy

--- interface/api/server.js
+++ interface/api/server.js
@@ -1,8 +1,20 @@
+const express = require("express")
 const open = require('amqplib').connect('amqp://rabbit')
 
 const mqCall = require("./funcs/mqCallGen")(open)
 
-mqCall("rpc_queue", "20").then(res => {
-  console.log(res)
+let app = express()
+
+app.get('/', (req,res) => {
+  mqCall("rpc_queue", "20").then(res => {
+    res.send(res)
+  })
 })
+
+app.get('/api', (req,res) => {
+  res.send("This is the what I meant to do")
+})
+
 console.log("hello")
+
+app.listen(80)
