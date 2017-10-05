ab102d1dfc7321c5a54330181799cde35fb15a38
JakobEng <jakobeng@fastmail.com>
2017-10-05 16:25:08 +0200

    decubled calling rabbitmq from main.go into a resueable function in rabbit/gen.go

--- server/cal/main.go
+++ server/cal/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	rabbit "app/rabbit"
 	"log"
 	"strconv"
 
@@ -28,64 +29,14 @@ func main() {
 	failOnError(err, "Failed to connect to RabbitMQ")
 	defer conn.Close()
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
+	mqCall := rabbit.Gen(conn)
 
-	q, err := ch.QueueDeclare(
-		"rpc_queue", // name
-		false,       // durable
-		false,       // delete when usused
-		false,       // exclusive
-		false,       // no-wait
-		nil,         // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
+	mqCall("rpc_queue", func(msg string) int {
+		n, err := strconv.Atoi(msg)
+		failOnError(err, "Failed to convert body to integer")
 
-	err = ch.Qos(
-		1,     // prefetch count
-		0,     // prefetch size
-		false, // global
-	)
-	failOnError(err, "Failed to set QoS")
+		log.Printf(" [.] fib(%d)", n)
+		return fib(n)
+	})
 
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		false,  // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	forever := make(chan bool)
-
-	go func() {
-		for d := range msgs {
-			n, err := strconv.Atoi(string(d.Body))
-			failOnError(err, "Failed to convert body to integer")
-
-			log.Printf(" [.] fib(%d)", n)
-			response := fib(n)
-
-			err = ch.Publish(
-				"",        // exchange
-				d.ReplyTo, // routing key
-				false,     // mandatory
-				false,     // immediate
-				amqp.Publishing{
-					ContentType:   "text/plain",
-					CorrelationId: d.CorrelationId,
-					Body:          []byte(strconv.Itoa(response)),
-				})
-			failOnError(err, "Failed to publish a message")
-
-			d.Ack(false)
-		}
-	}()
-
-	log.Printf(" [*] Awaiting RPC requests")
-	<-forever
 }
---
+++ server/cal/rabbit/gen.go
@@ -0,0 +1,86 @@
+package rabbit
+
+import (
+	"log"
+	"strconv"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func fib(n int) int {
+	if n == 0 {
+		return 0
+	} else if n == 1 {
+		return 1
+	} else {
+		return fib(n-1) + fib(n-2)
+	}
+}
+
+func Gen(conn *amqp.Connection) func(string, func(string) int) {
+
+	return func(que string, callback func(string) int) {
+		ch, err := conn.Channel()
+		failOnError(err, "Failed to open a channel")
+		defer ch.Close()
+
+		q, err := ch.QueueDeclare(
+			que,   // name
+			false, // durable
+			false, // delete when usused
+			false, // exclusive
+			false, // no-wait
+			nil,   // arguments
+		)
+		failOnError(err, "Failed to declare a queue")
+
+		err = ch.Qos(
+			1,     // prefetch count
+			0,     // prefetch size
+			false, // global
+		)
+		failOnError(err, "Failed to set QoS")
+
+		msgs, err := ch.Consume(
+			q.Name, // queue
+			"",     // consumer
+			false,  // auto-ack
+			false,  // exclusive
+			false,  // no-local
+			false,  // no-wait
+			nil,    // args
+		)
+		failOnError(err, "Failed to register a consumer")
+
+		forever := make(chan bool)
+
+		go func() {
+			for d := range msgs {
+				response := callback(string(d.Body))
+
+				err = ch.Publish(
+					"",        // exchange
+					d.ReplyTo, // routing key
+					false,     // mandatory
+					false,     // immediate
+					amqp.Publishing{
+						ContentType:   "text/plain",
+						CorrelationId: d.CorrelationId,
+						Body:          []byte(strconv.Itoa(response)),
+					})
+				failOnError(err, "Failed to publish a message")
+
+				d.Ack(false)
+			}
+		}()
+
+		log.Printf(" [*] Awaiting RPC requests")
+		<-forever
+	}
+}
