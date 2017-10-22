d7db6b19b7176fa3ada168941c36f918b86276d4
JakobEng <jakobeng@fastmail.com>
2017-10-22 19:20:41 +0200

    added jquery and a test ajax call to /api/login $.ajax({

--- interface/proxy/static/main.js
+++ interface/proxy/static/main.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+(function() {
+  $.ajax({
+    method: "POST",
+    url: "/api/login",
+    data: {info: "I'm doing something"},
+    success: function(data) {
+      console.log(data)
+    },
+    error: function(err) {
+      console.log(err)
+    }
+  })
+})()
